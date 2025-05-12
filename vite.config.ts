9463143540b444e9cd20619555484f1763bdcbcf
ahmed mohamed <ahmed@theportfolio.pro>
2025-05-13 01:19:18 +0300

    Initial Commit – Hebat East (BETA) version
    First upload of the website files . 
    This is the beta version Not the final design – some sections like the footer and layout elements are expected to change in the upcoming days.

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 1998, //My Brith Day Dont Care
+  },
+  plugins: [ // you can put your plug here or create one 
+    react(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
